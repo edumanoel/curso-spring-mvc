c7abb5680569c3414e86f1723e10e4ac867313a3
edumanoel <edumanoel@gmail.com>
2022-09-03 19:00:15 -0300

    first commit

---
+++ src/main/java/com/mbds/CursoSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.mbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbds/CursoSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
